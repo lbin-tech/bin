aec206115f62429888ca90f47f59e298edd8da9d
lbin-tech <lbin0810@aliyun.com>
2019-11-02 12:38:11 +0800

    把本地代码放到Git上

---
+++ git-test-project/src/main/java/com/wbin/FirstClass.java
@@ -0,0 +1,16 @@
+package com.wbin;
+
+/**
+ * @author lbin
+ * @date 2019/11/2 - 10:10
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("111111111111111111");
+        System.out.println("2222222222222222222");
+        System.out.println("33333333333333333");
+        System.out.println("4444444444444");
+        System.out.println("5555555555555555");
+        System.out.println("6666666666");
+    }
+}
